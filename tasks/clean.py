98f2760a3dbfc9114f0a59e0918b31886268252d
Oleksandr Foia <foia.oleksandr@gmail.com>
2025-11-17 22:59:47 +0200

    Fix poe tasks

--- tasks/clean.py
+++ tasks/clean.py
@@ -1,4 +1,8 @@
-import shutil, os;
+import shutil, os
 
-[shutil.rmtree(p, ignore_errors=True) for p in ('build','dist')];
+[shutil.rmtree(p, ignore_errors=True) for p in (
+    'build',
+    'dist',
+    'src/counter_app/ui_gen')]
+
 os.remove('counter-app.spec') if os.path.exists('counter-app.spec') else None
