dc78688ecce897b6700a6b5322bb1f74c2f73585
Oleksandr Foia <foia.oleksandr@gmail.com>
2025-11-18 16:15:10 +0200

    Fix pipeline

--- tasks/clean.py
+++ tasks/clean.py
@@ -3,6 +3,16 @@ import shutil, os
 [shutil.rmtree(p, ignore_errors=True) for p in (
     'build',
     'dist',
-    'src/counter_app/ui_gen')]
+    'src/counter_app/ui_gen',
+    'src/counter_app/web_view_gen',
+    'src/web_viewer/node_modules',
+    'src/web_viewer/web_dist',
+)]
 
-os.remove('counter-app.spec') if os.path.exists('counter-app.spec') else None
+for p in (
+        'src/web_viewer/package-lock.json',
+        'src/counter_app/_version.py',
+        'counter-app.spec'
+):
+    if os.path.exists(p):
+        os.remove(p)
